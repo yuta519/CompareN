3e154d9b2e103079b3d58ba92ac0ba462afbe817
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-23 20:52:48 +0900

    add backend directory of rust

---
+++ backend/target/debug/deps/backend-406a3ac07794ce73.d
@@ -0,0 +1,5 @@
+/Users/yuta519/work/CompareN/backend/target/debug/deps/backend-406a3ac07794ce73.rmeta: src/main.rs
+
+/Users/yuta519/work/CompareN/backend/target/debug/deps/backend-406a3ac07794ce73.d: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-6598aded1de1cc5e.d
@@ -0,0 +1,5 @@
+/Users/yuta519/work/CompareN/backend/target/debug/deps/backend-6598aded1de1cc5e.rmeta: src/main.rs
+
+/Users/yuta519/work/CompareN/backend/target/debug/deps/backend-6598aded1de1cc5e.d: src/main.rs
+
+src/main.rs:
