38f9d2e9b4bad3e60af0041cf3935bb3596d4107
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-10-24 09:39:16 +0900

    add presentation layerOC

---
+++ backend/src/commands.rs
@@ -0,0 +1,3 @@
+pub fn command_a() {
+    println!("executed command A")
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,11 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+mod presentation;
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+use actix_web::{web, App, HttpServer};
+use presentation::purpose::index::hello;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/hey", web::get().to(manual_hello)))
+    HttpServer::new(|| App::new().service(web::scope("/purpose").service(hello)))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
